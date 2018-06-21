a761bf9cd36096724b4368303d20ce524d100008
ahmaddregat <33619392+ahmaddregat@users.noreply.github.com>
2018-06-21 18:29:19 +0300

    Update TernaryHeap.java

--- TernaryHeap.java
+++ TernaryHeap.java
@@ -1,4 +1,20 @@
+ public class TernaryHeap {
 
-public class TernaryHeap {
+	public TernaryHeap(int capacity) {}
+
+	private int right(int i) {}
+
+	private int left(int i) {}
+
+	private int mid(int i) {}
+
+	private int parent(int i) {}
+	
+	public boolean insert(int k) {}
+
+	public int remove_max() {}
+
 
 }
+
+
