abe6cdbcdd0786a91f3fadc3b8fc3c03c7bde682
ahmaddregat <ahmad.dregat.97@hotmail.com>
2018-06-22 01:01:01 +0300

    adding helping function

--- TernaryHeap.java
+++ TernaryHeap.java
@@ -1,11 +1,11 @@
 /*
-*Author: ahmad dregat 
-*/
+ *Author: ahmad dregat 
+ */
 public class TernaryHeap {
-int d;
-int heap [];
-int size ;
-/** Constructor */
+	int d;
+	int heap [];
+	int size ;
+	/** Constructor */
 	public TernaryHeap(int capacity) {
 		this.d=d;
 		this.size=size;
@@ -26,16 +26,66 @@ int size ;
 	/** parent returns the parent of vertex  i*/
 	private int parent(int i) {
 		return((3*i)-1)/3;
-	  }
+	}
 	
-	public boolean insert(int k) {}
-/**return the max value in the heap **/
+	/**return the max value in the heap **/
 	private int getmax() {
 		return heap[0];
 	}
-	public int remove_max() {}
+	/** returns true if the heap is empty, otherwise false */
+	private boolean isEmpty() {
+		return size==0;
+	}
+	
+	/** 
+	 * The maxHeapfy maintains the Max-Heap Property
+	 * complexity is O(log(n))
+	 * @param heap - array of numbers
+	 * @param i - vertex 
+	 * @param Size - size of the heap
+	 * The function of maxHeapfy is to let the value of heap[i] "float-down"
+	 * in the Max-Heap so that subtree rooted at index i becomes a Max-Heap
+	 */
+	public void maxHeapify(int i,int size ) {
+		int larg ;
+		int left =left(i);
+		int right =right(i);
+		if (left <size&&heap[left]>heap[i]) {
+			larg=left;
+		}
+		else {
+			larg =i;
+			}
+		if (right<size && heap[right]>heap[larg]){
+			larg = right;
+		}
+		if (larg!=i){
+			swap(i, larg);
+			maxHeapify(larg, size);
+		}		
+	}
+		
+	private void swap(int i, int larg) {
+		// TODO Auto-generated method stub
+		int temp =heap[i];
+		heap[i]=heap[larg];
+		heap[larg]=temp ;
+		
+	}
 
+	
+	public boolean insert(int k) {
+		boolean flag=true;
+		return flag;
+	}
+	
+
+	public int remove_max() {
+		int max=0;
+		return max;}
 
 }
+
+
 
 
