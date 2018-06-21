55045a90860a1885625f0018de00542943e2983d
ahmaddregat <ahmad.dregat.97@hotmail.com>
2018-06-21 19:24:02 +0300

    Adding how to calculate the index of Children

--- TernaryHeap.java
+++ TernaryHeap.java
@@ -2,19 +2,37 @@
 *Author: ahmad dregat 
 */
 public class TernaryHeap {
-
-	public TernaryHeap(int capacity) {}
-
-	private int right(int i) {}
-
-	private int left(int i) {}
-
-	private int mid(int i) {}
-
-	private int parent(int i) {}
+int d;
+int heap [];
+int size ;
+/** Constructor */
+	public TernaryHeap(int capacity) {
+		this.d=d;
+		this.size=size;
+		heap=new int [capacity+1];
+	}
+	/** rightChild returns the right child of vertex  i*/
+	private int right(int i) {
+		return 3*i+1;
+	}
+	/** leftChild returns the left child of vertex  i*/
+	private int left(int i) {
+		return 3*i+3;
+	}
+	/** midChild returns the mid child of vertex  i*/
+	private int mid(int i) {
+		return 3*i+2;
+	}
+	/** parent returns the parent of vertex  i*/
+	private int parent(int i) {
+		return((3*i)-1)/3;
+	 }
 	
 	public boolean insert(int k) {}
-
+/**return the max value in the heap **/
+	private int getmax() {
+		return heap[0];
+	}
 	public int remove_max() {}
 
 
